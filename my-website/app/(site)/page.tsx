13761082aa2818f9909254d3e4994c9fa8248a0c
JjfyeDev <jye882@gmail.com>
2024-01-09 18:35:26 +0000

    Learn more added

--- my-website/app/(site)/page.tsx
+++ my-website/app/(site)/page.tsx
@@ -1,9 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className='text-center'>
       Hi I'm Jason.
+      <div className="py-4 text-xl">
+        <Link href="/about">
+          <div className='group relative hover:text-green-300 transition'>
+            Learn more
+            <span className="arrow rotate-[135deg] absolute top-[50%] left-full transform -translate-y-1/2 translate-x-[-70px] opacity-0 group-hover:opacity-100">
+              <span className="block w-3 h-3 border-r-2 border-b-2 border-green-300 animate-moving-arrow"></span>
+            </span>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
