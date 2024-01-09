32f48a22c0f8a30dc9d1a2d14c32cf2c80b65da0
JjfyeDev <jye882@gmail.com>
2024-01-09 02:16:00 +0000

    Arrow hover animation

--- my-website/components/TabItem.tsx
+++ my-website/components/TabItem.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
+
 interface SidebarItemProps {
   label: string;
   active?: boolean;
@@ -29,12 +30,19 @@ const TabItem: React.FC<SidebarItemProps> = ({
       transition
       text-neutral-400
       py-1
+      relative
+      group
       `,
       active && "text-green-300")}
     >
       <p className="truncate w-full">{label}</p>
+      
+      <span className="arrow rotate-[135deg] absolute top-[50%] left-full transform -translate-y-1/2 translate-x-[12px] opacity-0 group-hover:opacity-100">
+        <span className="block w-3 h-3 border-r-2 border-b-2 border-green-300 animate-moving-arrow"></span>
+      </span>
     </Link>
   );
 }
+
 
 export default TabItem
