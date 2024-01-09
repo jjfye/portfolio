57ab2962d3ab0a44cad4b1fbeacc797c664291b8
JjfyeDev <jye882@gmail.com>
2024-01-09 22:46:35 +0000

    Learn more arrow

--- my-website/app/(site)/page.tsx
+++ my-website/app/(site)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <Link href="/about">
           <div className='group relative hover:text-green-300 transition'>
             Learn more
-            <span className="arrow rotate-[135deg] absolute top-[50%] left-full transform -translate-y-1/2 translate-x-[-70px] opacity-0 group-hover:opacity-100">
+            <span className="arrow rotate-[135deg] absolute top-[50%] left-full transform -translate-y-1/2 translate-x-[-70px] group-hover">
               <span className="block w-3 h-3 border-r-2 border-b-2 border-green-300 animate-moving-arrow"></span>
             </span>
           </div>
