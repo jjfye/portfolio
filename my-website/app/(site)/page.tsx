9fead372312f8bd3b1d3a70b986026627b110e58
JjfyeDev <jye882@gmail.com>
2024-01-11 17:24:13 +0000

    Font sizes

--- my-website/app/(site)/page.tsx
+++ my-website/app/(site)/page.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="w-[60%]">
-      <div className='text-center'>
+      <div className='text-center text-3xl'>
         Hi I'm Jason.
-        <div className="py-4 text-xl">
+        <div className="py-4">
           <Link href="/about">
-            <div className='group hover:text-green-300 transition'>
+            <div className='group hover:text-green-300 transition text-xl'>
               Learn more
               <span className="arrow rotate-[135deg] absolute top-[50%]transform translate-y-[8px] translate-x-[14px] group-hover">
                 <span className="block w-3 h-3 border-r-2 border-b-2 border-green-300 animate-moving-arrow"></span>
