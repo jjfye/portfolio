fa9738366692ffb85e36f69dc478ac14e68289b2
JjfyeDev <jye882@gmail.com>
2024-01-11 17:24:06 +0000

    About page

---
+++ my-website/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+export default function About() {
+  return (
+    <div className="w-[60%]">
+      <div className='text-center text-3xl'>
+        ABOUT PAGE
+        <div className="py-4">
+        </div>
+      </div>
+      <div className="relative flex justify-center animate-moving-dog">
+        <img src="/images/Annoying_Dog_sprite.webp" alt="Undertale Annoying Dog Sprite" className="w-[20%] sm:w-[8%] h-auto translate-y-10 " />
+      </div>
+    </div>
+  )
+}
