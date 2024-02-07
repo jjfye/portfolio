65445ff3ce6dc183f14e5edeb970a12d9e68b98c
JjfyeDev <jye882@gmail.com>
2024-02-07 14:11:18 +0000

    Render dropdown

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,11 @@
+import MobileDropdownMenu from "@/components/MobileDropdownMenu";
 import Sidebar from "@/components/Sidebar";
 
 export default function Projects() {
   return (
     <div className="w-[80%]">
       <Sidebar/>
+      <MobileDropdownMenu/>
     </div>
   )
 }
